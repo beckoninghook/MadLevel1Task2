bf23692c18d6c48095d4aa9436d6b85d4b931f70
Niels van Oeffel <beckoninghook@gmail.com>
2020-08-31 22:07:17 +0200

    added logic

--- app/src/main/java/com/example/madlevel1task2/MainActivity.kt
+++ app/src/main/java/com/example/madlevel1task2/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.madlevel1task2
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.madlevel1task2.databinding.MainActivityBinding
+import kotlinx.android.synthetic.main.main_activity.*
 
 
 class MainActivity : AppCompatActivity()  {
@@ -18,5 +21,32 @@ class MainActivity : AppCompatActivity()  {
     }
 
     private fun initViews() {
+        val submitBtn = findViewById<Button>(R.id.submitBtn)
+        submitBtn.setOnClickListener(){
+            onSubmit()
+        }
+    }
+
+    private fun onSubmit(){
+        if (checkAnswer()){
+            Toast.makeText(applicationContext, "Correct Answer", Toast.LENGTH_LONG).show()
+        }else {
+            Toast.makeText(applicationContext, "Sumfin was wrong", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun checkAnswer(): Boolean {
+        if(textFieldRow1.text.toString() == "T"){
+            if(textFieldRow2.text.toString() == "F"){
+                if(textFieldRow3.text.toString() == "F"){
+                    if(textFieldRow4.text.toString() == "F"){
+                        return true
+                    }
+                }
+            }
+        } else {
+            return false
+        }
+        return false
     }
 }
